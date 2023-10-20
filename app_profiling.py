6c05f6b903f89711cdb406fc31c107cde58d8698
IAnthonyRenard <contact.anthony.renard@gmail.com>
2023-10-20 10:29:47 +0200

    Première version

---
+++ app_profiling.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import pandas as pd
+from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
+def main():
+
+    st.title("Analyse exploratoire simplifiée de ton fichier 🗓️")
+    st.subheader("Auteur : Anthony RENARD 🦊")
+
+
+    type=st.sidebar.selectbox("Choisissez votre type de fichier", ("Excel","CSV"))
+ 
+    if type=="Excel":
+
+        st.sidebar.header("Charger un fichier Excel 🗓️")
+        uploaded_file = st.sidebar.file_uploader("Sélectionnez un fichier Excel", type=["xls", "xlsx"])
+
+        if uploaded_file is not None:
+            
+            try:
+                df = pd.read_excel(uploaded_file) #A personnaliser si votre fichier excel contient des spécifités
+            except Exception as e:
+                st.sidebar.error(f"Erreur lors du chargement du fichier Excel ❌ : {str(e)}")
+                df = None
+
+            if df is not None:
+                
+                st.header("Aperçu des données")
+                st.write(df.head())
+
+                
+                st.header("Voici une analyse du contenu de ton fichier Excel 📊")
+                profile = ProfileReport(df, explorative=True)
+                st_profile_report(profile)
+
+    if type=="CSV":
+
+        st.sidebar.header("Charger un fichier CSV🗓️")
+        uploaded_file = st.sidebar.file_uploader("Sélectionnez un fichier Excel", type=["csv"])
+
+        if uploaded_file is not None:
+            
+            try:
+                df = pd.read_csv(uploaded_file) #A personnaliser si votre fichier csv contient des spécifités
+            except Exception as e:
+                st.sidebar.error(f"Erreur lors du chargement du fichier Excel ❌ : {str(e)}")
+                df = None
+
+            if df is not None:
+                
+                st.header("Aperçu des données")
+                st.write(df.head())
+
+                
+                st.header("Voici une analyse du contenu de ton fichier Excel 📊")
+                profile = ProfileReport(df, explorative=True)
+                st_profile_report(profile)
+    
+if __name__=='__main__':
+        main()
